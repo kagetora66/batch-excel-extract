ca7891ec8fc7228d766237f7839881b5ce58e953
sina <sina.safari1313@gmail.com>
2025-07-20 21:56:36 +0330

    added csv output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use csv::Writer;
 extern crate umya_spreadsheet;
 use std::fs::{self, File};
 use std::io::Write;
@@ -27,19 +28,20 @@ fn check_range(merged: &String, selected: &str) -> bool {
     }
 }
 //creates a vector of everything in the row
-fn get_row(row: u32, sheet: &Worksheet) -> Vec<String> {
-    
+ fn get_row(row: u32, sheet: &Worksheet) -> Vec<String> {    
     let mut row_values = Vec::new();
     let merged = sheet.get_merge_cells();
     let cell_row = row.to_string();
+    
     for range in merged{
+        let co
     let mut range_value = range.get_range();
     if check_range(&range_value, &cell_row) == true {
-        let mut merge_value = sheet.map_merged_cell(&*range_value);
-        let mut value = sheet.get_value(merge_value);
+        let mut merge_coord = sheet.map_merged_cell(&*range_value);
+        let mut value = sheet.get_value(merge_coord);
         row_values.push(value.to_string());
     }
-    }
+   }
     let cell = sheet.get_collection_by_row(&row);
     for item in cell {
         let value = item.get_cell_value().get_value();
@@ -47,8 +49,6 @@ fn get_row(row: u32, sheet: &Worksheet) -> Vec<String> {
     }
     row_values
 }
-
-
 
 fn get_keyword_coord(query: &str, sheet: &Worksheet) -> Vec<coordinates>
 {
@@ -70,14 +70,17 @@ fn main() {
     let path = std::path::Path::new("./smarts.xlsx");
     let mut book = umya_spreadsheet::reader::xlsx::read(path).unwrap();
     let sheet  = book.get_sheet_by_name("SMART Data").unwrap(); 
-    let keyword = "Total_LBAs_Written";
+    let keyword = "Wear_Leveling_Count";
     let coords = get_keyword_coord(&keyword, &sheet);
+    let mut wtr = Writer::from_path("output.csv").unwrap();
     for cord in coords {
         let row = get_row(cord.row, &sheet);
+        wtr.write_record(&row);
         println!("Row is:");
         for cell in row{
             println!("cell is: {}", cell);
         }
 
 }
+wtr.flush();
 }
